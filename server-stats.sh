020692fae20c00356ae2d13f5a3b47df7c6dd7a2
Chester <54451549+chesterik@users.noreply.github.com>
2026-01-31 02:03:05 +0200

    update

--- server-stats.sh
+++ server-stats.sh
@@ -14,8 +14,8 @@ df -h | grep "/dev/sda2" | awk '{printf "Used: %dGB / Total: %dGB (%.2f%%)\n", $
 
 echo -e "\n##########\n top 5 processes by CPU usage  \n##########"
 
-ps aux --sort=-%cpu | head -n 6
+ps aux --sort=-%cpu | head -n 6 | awk '{print $1 "\t" $2 "\t" $4 "\t" $11}'
 
 echo -e "\n##########\n top 5 processes by Memory usage  \n##########"
 
-ps aux --sort=-%mem | head -n 6
+ps aux --sort=-%mem | head -n 6 | awk '{print $1 "\t" $2 "\t" $4 "\t" $11}'
