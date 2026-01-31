613c775275c466ea36e7e9c13e764168e6e0c726
Chester <54451549+chesterik@users.noreply.github.com>
2026-01-31 02:13:09 +0200

    update

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Roadmap.sh Linux&Bash Projects
 
 1. [Server Performance Stats](https://roadmap.sh/projects/server-stats) ✅
+
+
+
+
+
+All projects can be found on [roadmap.sh](https://roadmap.sh/linux/projects)
--- server-stats.sh
+++ server-stats.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-echo -e "\n##########\n total cpu \n##########"
+echo -e "\n##########\n Total CPU usage \n##########"
 
 top -bn1 | grep "Cpu" | awk '{print $2 + $4}'
 
-echo -e "\n##########\n total memory \n##########"
+echo -e "\n##########\n Total memory usage (Free vs Used including percentage) \n##########"
 
 free -m | grep "Mem:" | awk '{printf "Used: %dMB / Total: %dMB (%.2f%%)\n", $3, $2, $3/$2*100}'
 
-echo -e "\n##########\n total disk usage \n##########"
+echo -e "\n##########\n Total disk usage (Free vs Used including percentage) \n##########"
 
 df -h | grep "/dev/sda2" | awk '{printf "Used: %dGB / Total: %dGB (%.2f%%)\n", $2, $3, $3/$2*100}'
 
-echo -e "\n##########\n top 5 processes by CPU usage  \n##########"
+echo -e "\n##########\n Top 5 processes by CPU usage \n##########"
 
 ps aux --sort=-%cpu | head -n 6 | awk '{print $1 "\t" $2 "\t" $4 "\t" $11}'
 
-echo -e "\n##########\n top 5 processes by Memory usage  \n##########"
+echo -e "\n##########\n Top 5 processes by memory usage \n##########"
 
 ps aux --sort=-%mem | head -n 6 | awk '{print $1 "\t" $2 "\t" $4 "\t" $11}'
